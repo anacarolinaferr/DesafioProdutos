8d168685c5f5cb7aa40652e5f58e4f6a44b670e5
anacfsoares <anacarol.fs@hotmail.com>
2021-09-19 20:39:07 -0300

    Initial commit

---
+++ src/main/java/com/desafioprodutos/DesafioProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.desafioprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProdutosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioProdutosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/desafioprodutos/DesafioProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProdutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
